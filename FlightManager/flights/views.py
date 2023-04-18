7a03c8aa66ce3dc9c3ceb9c35d57c91402643abd
MARTIKRISO <mshkodrov@gmail.com>
2023-04-18 15:08:07 +0300

    Finished project

--- FlightManager/flights/views.py
+++ FlightManager/flights/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.forms.models import model_to_dict
 from . import models
 import json
 from argon2 import PasswordHasher #Most secure hashing algo
+import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText
+
+
 ph = PasswordHasher()
 
 def index(request):
@@ -87,8 +93,11 @@ def reserve(request):
             ticket_type= item.get("ticket_type"),
             flight= flight,
         )
+        res_object.save()
+        send_email(model_to_dict(res_object))
         #print(res_object)
-       # res_object.save()
+        
+    
     return HttpResponse(200)
 
 def createUserRecord(request):
@@ -105,3 +114,40 @@ def createUserRecord(request):
     record.save()
     return HttpResponse(200)
 #TODO: Normal staff view using the login
+
+def send_email(data):
+    msg = MIMEMultipart()
+    msg['From'] = 'mshkodrov3@gmail.com'
+    msg['To'] = data["email"]
+    msg['Subject'] = 'Confirmation for booking on flight ' + str(data["flight"])
+    message = f"""
+    Dear Sir or Madam,
+    You are receiving this email to confirm your booking with Spas Airlines.
+    Booking info:
+
+    First Name: {data["f_name"]}
+    Middle Name: {data["m_name"]}
+    Last Name: {data["l_name"]}
+    EGN: {data["EGN"]}
+    Nationality: {data["nationality"]}
+    ticket_type: {data["ticket_type"]}
+
+    If the information is not correct, please contact us at contact@spasairlines.bg!
+
+
+    Thank you for choosing us!
+    """
+    msg.attach(MIMEText(message))
+
+    mailserver = smtplib.SMTP('smtp.gmail.com',587)
+    # identify ourselves to smtp gmail client
+    mailserver.ehlo()
+    # secure our email with tls encryption
+    mailserver.starttls()
+    # re-identify ourselves as an encrypted connection
+    mailserver.ehlo()
+    mailserver.login('mshkodrov3@gmail.com', 'gwimuxaworzrihve')
+
+    mailserver.sendmail('mshkodrov3@gmail.com', data["email"], msg.as_string())
+
+    mailserver.quit()
